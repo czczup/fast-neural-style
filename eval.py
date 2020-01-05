bc1d2df57153e15e4cd1d6da30b6ab9a1065400b
nonoka <wztxy89@163.com>
2020-01-05 22:50:44 +0800

    使用PIL读取图片，测试速度大幅提升，效果和原先使用的函数完全一致。

--- eval.py
+++ eval.py
@@ -6,6 +6,10 @@ import reader
 import model
 import time
 import os
+import numpy as np
+from PIL import Image
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '5'
 
 tf.app.flags.DEFINE_string('loss_model', 'vgg_16', 'The name of the architecture to evaluate. '
                            'You can view all the support models in nets/nets_factory.py')
@@ -17,58 +21,37 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def main(_):
-
-    # Get image's height and width.
-    height = 0
-    width = 0
-    with open(FLAGS.image_file, 'rb') as img:
-        with tf.Session().as_default() as sess:
-            if FLAGS.image_file.lower().endswith('png'):
-                image = sess.run(tf.image.decode_png(img.read()))
-            else:
-                image = sess.run(tf.image.decode_jpeg(img.read()))
-            height = image.shape[0]
-            width = image.shape[1]
-    tf.logging.info('Image size: %dx%d' % (width, height))
-
     with tf.Graph().as_default():
         with tf.Session().as_default() as sess:
 
-            # Read image data.
-            image_preprocessing_fn, _ = preprocessing_factory.get_preprocessing(
-                FLAGS.loss_model,
-                is_training=False)
-            image = reader.get_image(FLAGS.image_file, height, width, image_preprocessing_fn)
-
-            # Add batch dimension
-            image = tf.expand_dims(image, 0)
-
-            generated = model.net(image, training=False)
+            image_placeholder = tf.placeholder(tf.float32, [1, None, None, 3], name='input')/255.0
+            generated = model.net(image_placeholder, training=False)
             generated = tf.cast(generated, tf.uint8)
-
-            # Remove batch dimension
             generated = tf.squeeze(generated, [0])
 
-            # Restore model variables.
-            saver = tf.train.Saver(tf.global_variables(), write_version=tf.train.SaverDef.V1)
+            variables_to_restore = []
+            for v in tf.global_variables():
+                if not (v.name.startswith("vgg_16")):
+                    variables_to_restore.append(v)
+            variables_to_restore = [var for var in variables_to_restore if 'Adam' not in var.name]
+            saver = tf.train.Saver(variables_to_restore, max_to_keep=1)
+
             sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
-            # Use absolute path
+
             FLAGS.model_file = os.path.abspath(FLAGS.model_file)
             saver.restore(sess, FLAGS.model_file)
 
-            # Make sure 'generated' directory exists.
-            generated_file = 'generated/res.jpg'
             if os.path.exists('generated') is False:
                 os.makedirs('generated')
 
-            # Generate and write image data to file.
-            with open(generated_file, 'wb') as img:
+            im = Image.open("img/test.jpg")
+            for i in range(20):
                 start_time = time.time()
-                img.write(sess.run(tf.image.encode_jpeg(generated)))
+                image_ = sess.run(generated, feed_dict={image_placeholder:[np.array(im)]})
                 end_time = time.time()
                 tf.logging.info('Elapsed time: %fs' % (end_time - start_time))
-
-                tf.logging.info('Done. Please check %s.' % generated_file)
+            image_ = Image.fromarray(image_)
+            image_.save('generated/res.jpg')
 
 
 if __name__ == '__main__':
