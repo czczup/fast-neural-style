9ee583df1bcb4a9c1125099e3549dcc27fd21fe1
nonoka <wztxy89@163.com>
2020-01-05 23:03:30 +0800

    不用除以256了，除不除效果完全一样

--- eval.py
+++ eval.py
@@ -8,9 +8,7 @@ from PIL import Image
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '5'
 
-tf.app.flags.DEFINE_string('loss_model', 'vgg_16', 'The name of the architecture to evaluate. '
-                           'You can view all the support models in nets/nets_factory.py')
-tf.app.flags.DEFINE_integer('image_size', 256, 'Image size to train.')
+tf.app.flags.DEFINE_string('loss_model', 'vgg_16', "")
 tf.app.flags.DEFINE_string("model_file", "models.ckpt", "")
 tf.app.flags.DEFINE_string("image_file", "a.jpg", "")
 
@@ -21,14 +19,14 @@ def main(_):
     with tf.Graph().as_default():
         with tf.Session().as_default() as sess:
 
-            image_placeholder = tf.placeholder(tf.float32, [1, None, None, 3], name='input')/255.0
+            image_placeholder = tf.placeholder(tf.float32, [1, None, None, 3], name='input')
             generated = model.net(image_placeholder, training=False)
             generated = tf.cast(generated, tf.uint8)
             generated = tf.squeeze(generated, [0])
 
             variables_to_restore = []
             for v in tf.global_variables():
-                if not (v.name.startswith("vgg_16")):
+                if not (v.name.startswith(FLAGS.loss_model)):
                     variables_to_restore.append(v)
             variables_to_restore = [var for var in variables_to_restore if 'Adam' not in var.name]
             saver = tf.train.Saver(variables_to_restore, max_to_keep=1)
@@ -41,10 +39,10 @@ def main(_):
             if os.path.exists('generated') is False:
                 os.makedirs('generated')
 
-            im = Image.open("img/test.jpg")
+            im = Image.open(FLAGS.image_file)
             for i in range(20):
                 start_time = time.time()
-                image_ = sess.run(generated, feed_dict={image_placeholder:[np.array(im)]})
+                image_ = sess.run(generated, feed_dict={image_placeholder: [np.array(im)]})
                 end_time = time.time()
                 tf.logging.info('Elapsed time: %fs' % (end_time - start_time))
             image_ = Image.fromarray(image_)
